b235922faa2934cc374548a69ca4dcd06b25b539
root <root@DESKTOP-0B39G0E>
2023-08-28 16:19:38 +0200

    vision_based

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from torch import nn
 from gym import spaces
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
-class CNN(BaseFeaturesExtractor):
+class CustomCNN(BaseFeaturesExtractor):
     def __init__(self, observation_space: spaces.Box, features_dim: int = 512):
         super().__init__(observation_space, features_dim)
         print("Init CNN")
@@ -41,3 +41,19 @@ class ResNet18(BaseFeaturesExtractor):
 
     def forward(self, observations: th.Tensor) -> th.Tensor:
         return self.resnet(observations)
+    
+class CustomResNet18(BaseFeaturesExtractor):
+    def __init__(self, observation_space: spaces.Box, features_dim: int = 128):
+
+        super().__init__(observation_space, features_dim)
+        print("Init CustomResNet18")
+        n_input_channels = observation_space.shape[0]
+        self.net = resnet18(weights=ResNet18_Weights)
+        self.net.conv1 = nn.Sequential(
+                            nn.Conv2d(n_input_channels, 64, kernel_size = 7, stride = 2, padding = 3),
+                            nn.BatchNorm2d(64),
+                            nn.ReLU())
+        self.net.fc = nn.Linear(in_features=512, out_features=128)
+
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        return self.net(observations)
--- train_vision_based.py
+++ train_vision_based.py
@@ -1,80 +1,146 @@
-from wrappers import *
-#from gym.wrappers.pixel_observation import PixelObservationWrapper
-# from wrappers import Grayscale
-# from gym.wrappers.resize_observation import ResizeObservation
-# from gym.wrappers.frame_stack import FrameStack
-# from gym.wrappers.gray_scale_observation import GrayScaleObservation
+from wrappers import ImageObservationWrapper
+from gym.wrappers.pixel_observation import PixelObservationWrapper
+from gym.wrappers.resize_observation import ResizeObservation
+from gym.wrappers.frame_stack import FrameStack
+from gym.wrappers.gray_scale_observation import GrayScaleObservation
 from env.custom_hopper import *
-from models import CNN
-from policy_gradient import init_model
-from evaluate_policy import evaluate_policy
+from models import CustomCNN
+from models import CustomResNet18
+from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3 import PPO
+import os
 
+N_TIMESTEPS = 250_000
 
-def custom_extractor(model, n_features):
-    
-    """
-    It will create custom feature extractor for cnn networks.
-    
-    :param model: feature extractor class that we want to use
-    :param n_features: number of output features
-    :type model: class
-    :type n_features: int
-    :return: kwargs of the model
-    """
-    
-    kwargs = dict(
-        features_extractor_class=model,
-        features_extractor_kwargs=dict(features_dim=n_features)
-    )
-        
-    return kwargs
-
-N_TIMESTEPS = 100_000
-
-def train_vision_based(params):
-    source_domain = params['train_domain']
-
-    env = gym.make(f"CustomHopper-{source_domain}-v0")
-    print(env.observation_space)
+def vision_based_env(env):
     env = PixelObservationWrapper(env)
     print('state observation shape', env.observation_space.shape)
-    env = PreprocessWrapper(env)
+    env = ImageObservationWrapper(env)
     print('obervation shape after preprocessing:', env.observation_space.shape)
-            
-    env = GrayScaleWrapper(env, smooth=False, preprocessed=True, keep_dim=False)
+    env = ResizeObservation(env, shape=[224, 224])
+    print('obervation shape after resizing:', env.observation_space.shape)        
+    env = GrayScaleObservation(env, keep_dim=False)
     print('obervation shape after gray scaling:', env.observation_space.shape)
-    env = ResizeWrapper(env, shape=[240, 240])
-    print('obervation shape after resizing:', env.observation_space.shape)
-
     env = FrameStack(env, num_stack=4)
     print('obervation shape after stacking:', env.observation_space.shape)
+    return env
 
-    policy_kwargs = dict(
-        features_extractor_class=CNN, # to use other model: CustomCNN
-        features_extractor_kwargs=dict(features_dim = 128), # was 128 in oldcnn
-    )
+def train_vision_based(params):
 
-    model = PPO("CnnPolicy", env, policy_kwargs = policy_kwargs, verbose = 1, batch_size = 32, learning_rate = 0.0003)
-    model.learn(N_TIMESTEPS, progress_bar=True)
-    # masses_distribution = env.get_stats()[0]
+    env = gym.make("CustomHopper-source-v0")
+    print(env.observation_space)
+    env = vision_based_env(env)
+    target_env = gym.make('CustomHopper-target-v0')
+    print(target_env.observation_space)
+    target_env = vision_based_env(target_env)
+    udr = ""
+    if params['udr_enable']:
+        masses_to_set = env.get_parameters()[1:]
+        lower_bound = masses_to_set * (1 - 0.5 * 0.7)
+        upper_bound = masses_to_set * (1 + 0.5 * 0.7)
+        distribution_set = [lower_bound[0], upper_bound[0], lower_bound[1], upper_bound[1], lower_bound[2],
+                            upper_bound[2]]
+        env.set_udr_training(True)
+        env.set_udr_distribution(distribution_set)
+        udr = "_UDR"
 
-    # with open('masses_distribution.csv', 'w', newline='') as csvfile:
-    #     my_writer = csv.writer(csvfile)
-    #     my_writer.writerows(masses_distribution)
+    policy_kwargs = None
+    lr = ''
+    if params['learning_rate'] == 1e-3:
+        lr = '0'
+    else:
+        lr = '1'
 
-    model.save("./experiments/vision_based/model")
+    if params['vision_based_model'] == "customCNN":
+        policy_kwargs = dict(
+            features_extractor_class=CustomCNN, # to use other model: CustomCNN
+            features_extractor_kwargs=dict(features_dim = 128), # was 128 in oldcnn
+        )
+    elif params['vision_based_model'] == "resnet18":
+        policy_kwargs = dict(
+            features_extractor_class=CustomResNet18, # to use other model: CustomCNN
+            features_extractor_kwargs=dict(features_dim = 128), # was 128 in oldcnn
+        )
+    if os.path.exists(f"./training/models/vision_based/{params['vision_based_model']}_LR_{lr}" + udr + ".zip"):
+                    print("Found model!")
+                    model = PPO.load(f"./training/models/vision_based/{params['vision_based_model']}_LR_{lr}" + udr, env=env)
+    else:
+        model = PPO("CnnPolicy", env, policy_kwargs = policy_kwargs, verbose = 1, learning_rate = params['learning_rate'])
+        model.learn(N_TIMESTEPS, progress_bar=True)
+        model.save(f"./training/models/vision_based/{params['vision_based_model']}_LR_{lr}" + udr)
+    
+    print("Source-Source environment results:")
+    res = evaluate_policy(model, env, n_eval_episodes=50, render=False)
+    print(res)
+    print("Source-Target environment results:")
+    res = evaluate_policy(model, target_env, n_eval_episodes=50, render=False)
+    print(res)
+    env.close()
+    target_env.close()
 
 if __name__ == "__main__":
-    print(gym.__version__)
-
     params = {
-        'train_domain': 'source'
+        'train_domain': 'source',
+        'udr_enable': False,
+        'vision_based_model': 'customCNN',
+        'learning_rate': 1e-3
     }
-    train_vision_based(params)
-    print("Training finished")
+    print("training with params = ", params)
+    train_vision_based(params=params)
     params = {
-                'evaluate_domain': 'target',
-                'model_path': "./experiments/vision_based/model/hopper.zip",
-            }
-    evaluate_policy(params=params)
+        'train_domain': 'source',
+        'udr_enable': False,
+        'vision_based_model': 'customCNN',
+        'learning_rate': 3e-4
+    }
+    print("training with params = ", params)
+    train_vision_based(params=params)
+    params = {
+        'train_domain': 'source',
+        'udr_enable': True,
+        'vision_based_model': 'customCNN',
+        'learning_rate': 1e-3
+    }
+    print("training with params = ", params)
+    train_vision_based(params=params)
+    params = {
+        'train_domain': 'source',
+        'udr_enable': True,
+        'vision_based_model': 'customCNN',
+        'learning_rate': 3e-4
+    }
+    print("training with params = ", params)
+    train_vision_based(params=params)
+    params = {
+        'train_domain': 'source',
+        'udr_enable': False,
+        'vision_based_model': 'resnet18',
+        'learning_rate': 1e-3
+    }
+    print("training with params = ", params)
+    train_vision_based(params=params)
+    params = {
+        'train_domain': 'source',
+        'udr_enable': False,
+        'vision_based_model': 'resnet18',
+        'learning_rate': 3e-4
+    }
+    print("training with params = ", params)
+    train_vision_based(params=params)
+    params = {
+        'train_domain': 'source',
+        'udr_enable': True,
+        'vision_based_model': 'resnet18',
+        'learning_rate': 1e-3
+    }
+    print("training with params = ", params)
+    train_vision_based(params=params)
+    params = {
+        'train_domain': 'source',
+        'udr_enable': True,
+        'vision_based_model': 'resnet18',
+        'learning_rate': 3e-4
+    }
+    print("training with params = ", params)
+    train_vision_based(params=params)
+    
